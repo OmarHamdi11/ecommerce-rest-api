94bbeab7f5f991665cf71040b6692efc706a0399
OmarHamdi11 <omarellafy1@gmail.com>
2025-11-20 20:10:00 +0200

    make a global response for the hole project

---
+++ src/main/java/com/example/ecommerce_rest_api/response/PageResponse.java
@@ -0,0 +1,33 @@
+package com.example.ecommerce_rest_api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponse<T> {
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+    private boolean first;
+    private List<T> pageContent;
+
+    public PageResponse(Page<T> page) {
+        this.pageContent = page.getContent();
+        this.pageNo = page.getNumber();
+        this.pageSize = page.getSize();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+        this.last = page.isLast();
+        this.first = page.isFirst();
+    }
+}
